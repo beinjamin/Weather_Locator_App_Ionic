b72d489866e1694d225582adf0948c7c0bb97860
beinjamin <jauresbeinjamin@gmail.com>
2022-04-11 20:01:04 +0100

    Insertion des element du tableaux  title , url , icon

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -8,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class MenuPage implements OnInit {
   public menus=[
     {
-      title:"Home" , url:"menu/home" ,icon:
-    }
+      title:"Home" , url:"menu/home" ,icon: 'share'
+    },
     {
+      title:"Meteo" , url:"menu/meteo" ,icon: 'snow'
+    },
+    {
+      title:"Gallery" , url:"menu/gallery" ,icon: 'school'
+    },
+    {
+      title:"Location" , url:"menu/location" ,icon: 'sync'
+    },
+    {
+      title:"Logout" , url:"/login" ,icon: 'logout'
+    },
 
-    }
   ]
 
   constructor() { }
