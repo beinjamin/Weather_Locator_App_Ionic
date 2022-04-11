66a8e8d48034ff807f865345ff877ceb203312d4
beinjamin <jauresbeinjamin@gmail.com>
2022-04-11 20:15:58 +0100

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,27 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  {
-    path: 'meteo',
-    loadChildren: () => import('./meteo/meteo.module').then( m => m.MeteoPageModule)
-  },
-  {
-    path: 'gallery',
-    loadChildren: () => import('./gallery/gallery.module').then( m => m.GalleryPageModule)
-  },
-  {
-    path: 'locations',
-    loadChildren: () => import('./locations/locations.module').then( m => m.LocationsPageModule)
-  },
+
   {
     path: 'menu',
     loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
