fd369e7ffb5683d33b1579c47e038b5d042004ac
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 13:54:35 +0100

    Update

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -7,36 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { MenuPageRoutingModule } from './menu-routing.module';
 
 import { MenuPage } from './menu.page';
-import { Routes } from '@angular/router';
-const routes: Routes = [
 
-
-{
-  path: '',
-  component: MenuPage,
-  children: [
-    {
-      path: 'home',
-      loadChildren: () => import('../home/home.module').then( m => m.HomePageModule)
-    },
-    {
-      path: 'meteo',
-      loadChildren: () => import('../meteo/meteo.module').then( m => m.MeteoPageModule)
-    },
-    {
-      path: 'gallery',
-      loadChildren: () => import('../gallery/gallery.module').then( m => m.GalleryPageModule)
-    },
-    {
-      path: 'locations',
-      loadChildren: () => import('../locations/locations.module').then( m => m.LocationsPageModule)
-    },
-
-
-  ]
-
-  },
-];
 @NgModule({
   imports: [
     CommonModule,
